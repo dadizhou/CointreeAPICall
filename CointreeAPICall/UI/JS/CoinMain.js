6c12961fa2d52638be16433427be84870b992fef
Dadi Zhou <dadi.zhou@gmail.com>
2021-03-27 18:48:26 +1100

    Adding UI logic for coin price update

--- CointreeAPICall/UI/JS/CoinMain.js
+++ CointreeAPICall/UI/JS/CoinMain.js
@@ -13,6 +13,10 @@ function prepareDefaultCoin(selectedCoinSymbol) {
     this.setState({ selectedCoinSymbol });
 }
 
+function updatePriceDetails(priceDetails) {
+    this.setState({ priceDetails });
+}
+
 class CurrentPreferredCoin extends React.Component {
     constructor(props) {
         super(props);
@@ -101,6 +105,86 @@ class SetDefaultCoinButton extends React.Component {
     }
 }
 
+class CoinPriceDetails extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            priceDetails: {
+                ask: 0,
+                bid: 0,
+                rate: 0,
+                priceChange: {
+                    ask: 0,
+                    bid: 0,
+                    rate: 0
+                },
+                messages: []
+            }
+        };
+
+        updatePriceDetails = updatePriceDetails.bind(this);
+    }
+
+    render() {
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Type</th>
+                        <th>Current Price</th>
+                        <th>Changed from Previous Enquiry (%)</th>
+                    </tr>
+                    <tr>
+                        <td>Ask</td>
+                        <td>{this.state.priceDetails.ask}</td>
+                        <td>{this.state.priceDetails.priceChange.ask}</td>
+                    </tr>
+                    <tr>
+                        <td>Bid</td>
+                        <td>{this.state.priceDetails.bid}</td>
+                        <td>{this.state.priceDetails.priceChange.bid}</td>
+                    </tr>
+                    <tr>
+                        <td>Rate</td>
+                        <td>{this.state.priceDetails.rate}</td>
+                        <td>{this.state.priceDetails.priceChange.rate}</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class UpdatePriceDetailsButton extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            enquiryResult: null
+        };
+    }
+
+    GetPriceDetails = (e) => {
+        fetch("https://localhost:5001/api/CointreeAPICall/EnquireCoinPriceDetails",
+            {
+                mode: "cors",
+                headers: { "Access-Control-Allow-Origin": "*" }
+            })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ enquiryResult: data });
+                updatePriceDetails(this.state.enquiryResult);
+            });
+    };
+
+    render() {
+        return (
+            <button onClick={this.GetPriceDetails}>Update Price</button>
+        );
+    }
+}
+
 class SetDefaultCoin extends React.Component {
     constructor(props) {
         super(props);
@@ -129,6 +213,8 @@ class SetDefaultCoin extends React.Component {
                 <CurrentPreferredCoin />
                 <CoinDropDown coinListFromParent={this.state.coinList} defaultCoin={GetDefaultCoin(this.state.coinList)} />
                 <SetDefaultCoinButton />
+                <CoinPriceDetails />
+                <UpdatePriceDetailsButton />
             </div>
         );
     }
